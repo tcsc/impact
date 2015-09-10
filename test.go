9d035959b02a344d02f5682a7dbdc018fbb6dc4b
Trent Clarke <trent.clarke@gmail.com>
2015-09-10 22:58:17 +1000

    Tweaks

--- test.go
+++ test.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"os"
 	"os/exec"
+	"os/signal"
 	"path"
 	"path/filepath"
+	"strings"
+	"syscall"
 )
 
 var prePatchTestFailed = errors.New("pre test failed")
 var postPatchTestFailed = errors.New("post test failed")
 
-func fetchCode(p pkg, dir string, env []string) error {
-	fmt.Printf("%04d: Fetching code...\n", p.index)
+func fetchCode(idx int, p pkg, dir string, env []string) error {
+	fmt.Printf("%04d: %d Fetching code...\n", p.index, idx)
 	get := exec.Command("go", "get", "-t", p.slug)
 	get.Env = env
 	get.Stdout = os.Stdout
@@ -28,7 +32,7 @@ func runTests(p pkg, logfile, dir string, env []string) error {
 	}
 	defer file.Close()
 
-	test := exec.Command("go", "test", "-v", fmt.Sprintf("%s/...", p.slug))
+	test := exec.Command("go", "test", "-v", p.slug)
 	test.Stdout = file
 	test.Env = env
 
@@ -50,8 +54,8 @@ func applyPatch(patchFile, dir string) error {
 	return cmd.Run()
 }
 
-func quickCheck(p pkg, dir string) error {
-	fmt.Printf("%04d: Checking out %s into %s\n", p.index, p.slug, dir)
+func quickCheck(idx int, p pkg, dir string) error {
+	fmt.Printf("%04d: %d Checking out %s into %s\n", p.index, idx, p.slug, dir)
 	err := os.Mkdir(dir, 0755)
 	if err != nil {
 		return err
@@ -60,35 +64,35 @@ func quickCheck(p pkg, dir string) error {
 	env := os.Environ()
 	env = append(env, fmt.Sprintf("GOPATH=%s", dir))
 
-	err = fetchCode(p, dir, env)
+	err = fetchCode(idx, p, dir, env)
 	if err != nil {
-		fmt.Printf("%04d: Failed to fetch code (counds as a pre-patch failure): %s\n",
-			p.index, err.Error())
+		fmt.Printf("%04d: %d Failed to fetch code (counds as a pre-patch failure): %s\n",
+			p.index, idx, err.Error())
 		return prePatchTestFailed
 	}
 
-	fmt.Printf("%04d: Running pre-patch tests\n", p.index)
+	fmt.Printf("%04d: %d Running pre-patch tests\n", p.index, idx)
 	err = runTests(p, "pre-test.log", dir, env)
 	if err != nil {
-		fmt.Printf("%04d: Failed pre-patch tests. No further testing.\n", p.index)
+		fmt.Printf("%04d: %d Failed pre-patch tests. No further testing.\n", p.index, idx)
 		return prePatchTestFailed
 	}
 
-	fmt.Printf("%04d: Applying patch\n", p.index)
+	fmt.Printf("%04d: %d Applying patch\n", p.index, idx)
 	err = applyPatch("mock.patch", dir)
 	if err != nil {
-		fmt.Printf("%04d: Failed to apply patch. Bailing our.\n", p.index)
+		fmt.Printf("%04d: %d Failed to apply patch. Bailing our.\n", p.index, idx)
 		return err
 	}
 
-	fmt.Printf("%04d: Running post-patch tests\n", p.index)
+	fmt.Printf("%04d: %d Running post-patch tests\n", p.index, idx)
 	err = runTests(p, "post-test.log", dir, env)
 	if err != nil {
-		fmt.Printf("%04d: Failed post-patch tests: %s.\n", p.index, err.Error())
+		fmt.Printf("%04d: %d Failed post-patch tests: %s.\n", p.index, idx, err.Error())
 		return postPatchTestFailed
 	}
 
-	fmt.Printf("%04d: Passed.\n", p.index)
+	fmt.Printf("%04d: %d Passed.\n", p.index, idx)
 
 	return nil
 }
@@ -103,18 +107,37 @@ type reply struct {
 	err error
 }
 
+func loadPackageList(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	pkgs := make([]string, 0)
+	s := bufio.NewScanner(file)
+	for s.Scan() {
+		if s.Err() != nil {
+			return nil, err
+		}
+		pkgs = append(pkgs, strings.TrimSpace(s.Text()))
+	}
+
+	return pkgs, nil
+}
+
 func main() {
-	packages := []string{
-		"github.com/BlueDragonX/dockerclient/mockclient",
-		"github.com/CenturyLinkLabs/watchtower/container/mockclient",
-		"github.com/CiscoCloud/mesos-consul/mesos/zkdetect",
-		"github.com/yieldr/go-log/log/logstream",
-		"github.com/Hearst-DD/goib/mocks",
+	packages, err := loadPackageList("pkgs.txt")
+	if err != nil {
+		fmt.Printf("Failed to load pkgs: %s\n", err.Error())
+		os.Exit(1)
 	}
 
 	pkgChan := make(chan pkg, 10)
 	rpyChan := make(chan reply, 10)
-	done := make(chan bool, 1)
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt)
 
 	passed := make([]pkg, 0)
 	failedPreTest := make([]pkg, 0)
@@ -125,6 +148,8 @@ func main() {
 		replies := 0
 		for reply := range rpyChan {
 			pkgInfo := pkg{index: reply.index, slug: reply.slug}
+			fmt.Printf("%04d: Processing result\n", reply.index)
+
 			switch reply.err {
 			case prePatchTestFailed:
 				failedPreTest = append(failedPreTest, pkgInfo)
@@ -144,29 +169,30 @@ func main() {
 
 			replies++
 			if replies == len(packages) {
-				done <- true
+				fmt.Println("Signalling completion")
+				done <- syscall.SIGQUIT
 				break
 			}
 		}
 	}
 	go collate()
 
-	test := func() {
+	test := func(i int) {
 		for pkgInfo := range pkgChan {
 			rpy := reply{pkg: pkgInfo, err: nil}
 
 			workdir, err := filepath.Abs(fmt.Sprintf("%04d", pkgInfo.index))
 			if err == nil {
-				err = quickCheck(pkgInfo, workdir)
+				err = quickCheck(i, pkgInfo, workdir)
 			}
 			rpy.err = err
 			rpyChan <- rpy
 		}
 	}
-	go test()
-	go test()
-	go test()
-	go test()
+
+	for i := 0; i < 8; i++ {
+		go test(i)
+	}
 
 	for i, slug := range packages {
 		pkgChan <- pkg{index: i, slug: slug}
